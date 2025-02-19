0efdb96d4156af8cb02b8a9e29f9d548bde32cf5
jawn <rodriguez.jawn@gmail.com>
2025-02-19 00:02:45 -0800

    update solution for 2024 day 4

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -46,13 +46,17 @@ func main() {
 
 	// The word search grid is assumed to be nonempty and rectangular
 	// The word is assumed to be at least 2 characters
-	count := 0
+	word_count := 0
+	pattern_count := 0
 	height := len(word_search)
 	width := len(word_search[0])
 	word := "XMAS"
 	word_chars := strings.Split(word, "")
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
+			if hasXMASPattern(word_search, i, j) {
+				pattern_count += 1
+			}
 			if word_search[i][j] != word_chars[0] {
 				continue
 			}
@@ -70,7 +74,7 @@ func main() {
 					}
 					// Increment count if the entire word has been found
 					if k == len(word_chars)-1 {
-						count += 1
+						word_count += 1
 					}
 					new_row += delta.Dy
 					new_col += delta.Dx
@@ -79,5 +83,55 @@ func main() {
 		}
 	}
 
-	fmt.Printf("The word '%s' was found %d times in the word search\n", word, count)
+	fmt.Printf("The word '%s' was found %d times in the word search\n", word, word_count)
+	fmt.Printf("The X-MAS pattern was found %d times\n", pattern_count)
+}
+
+func hasXMASPattern(word_search [][]string, row int, col int) bool {
+	if word_search[row][col] != "A" {
+		return false
+	}
+
+	height := len(word_search)
+	width := len(word_search[0])
+
+	var new_row, new_col int
+
+	// Set upper left corner
+	new_row, new_col = row+Directions["UP_LEFT"].Dy, col+Directions["UP_LEFT"].Dx
+	if new_row < 0 || new_row >= height || new_col < 0 || new_col >= width {
+		return false
+	}
+	upper_left := word_search[new_row][new_col]
+
+	// Set upper right corner
+	new_row, new_col = row+Directions["UP_RIGHT"].Dy, col+Directions["UP_RIGHT"].Dx
+	if new_row < 0 || new_row >= height || new_col < 0 || new_col >= width {
+		return false
+	}
+	upper_right := word_search[new_row][new_col]
+
+	// Set lower left corner
+	new_row, new_col = row+Directions["DOWN_LEFT"].Dy, col+Directions["DOWN_LEFT"].Dx
+	if new_row < 0 || new_row >= height || new_col < 0 || new_col >= width {
+		return false
+	}
+	lower_left := word_search[new_row][new_col]
+
+	// Set lower right corner
+	new_row, new_col = row+Directions["DOWN_RIGHT"].Dy, col+Directions["DOWN_RIGHT"].Dx
+	if new_row < 0 || new_row >= height || new_col < 0 || new_col >= width {
+		return false
+	}
+	lower_right := word_search[new_row][new_col]
+
+	// Check if opposite corners are a pair of "M" and "S"
+	if !(upper_left == "M" && lower_right == "S") && !(upper_left == "S" && lower_right == "M") {
+		return false
+	}
+	if !(upper_right == "M" && lower_left == "S") && !(upper_right == "S" && lower_left == "M") {
+		return false
+	}
+
+	return true
 }
