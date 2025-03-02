86ced1164154ebc0d2ed299394391dbe3283f50e
jawn <rodriguez.jawn@gmail.com>
2025-03-02 12:27:38 -0800

    add solution for 2024 day 10

---
+++ 2024/day10/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	Row int
+	Col int
+}
+
+func (p Point) Add(other Point) Point {
+	return Point{
+		Row: p.Row + other.Row,
+		Col: p.Col + other.Col,
+	}
+}
+
+var Dir = map[string]Point{
+	"UP":    {Row: -1, Col: 0},
+	"RIGHT": {Row: 0, Col: 1},
+	"DOWN":  {Row: 1, Col: 0},
+	"LEFT":  {Row: 0, Col: -1},
+}
+
+var Endpoints = map[Point][]Point{}
+
+func main() {
+	filename := "input.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Could not open %s: %v\n", filename, err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Printf("Reading topographic map...\n")
+	grid := [][]int{}
+	trailheads := []Point{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_splice := strings.Split(line, "")
+		row := []int{}
+		for i := 0; i < len(line_splice); i++ {
+			num, err := strconv.Atoi(line_splice[i])
+			if err != nil {
+				fmt.Printf("String conversion error: %v", err)
+			}
+			if num == 0 { // Locate potential trailheads
+				p := Point{Row: len(grid), Col: i}
+				trailheads = append(trailheads, p)
+			}
+			row = append(row, num)
+		}
+		grid = append(grid, row)
+	}
+
+	fmt.Printf("Calculating trail scores...\n")
+	total := 0
+	for i := 0; i < len(trailheads); i++ {
+		Endpoints[trailheads[i]] = []Point{}
+		total += followPath(grid, trailheads[i], trailheads[i], 0)
+	}
+
+	fmt.Printf("Trail scores total: %d\n", total)
+}
+
+func followPath(grid [][]int, trailhead Point, current_point Point, current_elevation int) int {
+	score := 0
+
+	for _, delta := range Dir {
+		next_point := current_point.Add(delta)
+		if !isOutOfBounds(grid, next_point) {
+			next_elevation := grid[next_point.Row][next_point.Col]
+			if current_elevation == 8 && next_elevation == 9 {
+				if !isInSlice(next_point, Endpoints[trailhead]) {
+					score += 1
+					Endpoints[trailhead] = append(Endpoints[trailhead], next_point)
+				}
+			} else if next_elevation == current_elevation+1 {
+				score += followPath(grid, trailhead, next_point, next_elevation)
+			}
+		}
+	}
+
+	return score
+}
+
+func isOutOfBounds(grid [][]int, p Point) bool {
+	grid_height := len(grid)
+	grid_width := len(grid[0])
+
+	if p.Row < 0 || p.Row >= grid_height || p.Col < 0 || p.Col >= grid_width {
+		return true
+	}
+
+	return false
+}
+
+func isInSlice[T comparable](val T, slice []T) bool {
+	for _, v := range slice {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
