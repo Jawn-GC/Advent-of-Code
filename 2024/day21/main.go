11f4a866d20c33fada6f6f588014e75819ed0df8
jawn <rodriguez.jawn@gmail.com>
2025-03-14 15:26:01 -0700

    update solution for 2024 day 21

--- 2024/day21/main.go
+++ 2024/day21/main.go
@@ -96,17 +96,23 @@ func main() {
 	// The user is at a direction pad
 	fmt.Printf("Writing instructions...\n")
 	complexity_sum := 0
+	num_dir_robots := 2
+	dir_memo := make(map[string]string) // Key: "start_symbol"+"end_symbol"
 	for _, code := range codes {
-		robot1 := num_ref["A"]
-		robot2 := dir_ref["A"]
-		robot3 := dir_ref["A"]
+		main_robot := num_ref["A"]
+		dir_robots := []Point{}
+		instructions := numToDir(&main_robot, code)
 
-		r2_code := numToDir(&robot1, code)
-		r3_code := dirToDir(&robot2, r2_code)
-		user_code := dirToDir(&robot3, r3_code)
+		for i := 0; i < num_dir_robots; i++ {
+			dir_robots = append(dir_robots, dir_ref["A"])
+		}
+
+		for _, robot := range dir_robots {
+			instructions = dirToDir(&robot, instructions, dir_memo)
+		}
 
 		num, _ := strconv.Atoi(strings.TrimRight(code, "A"))
-		complexity_sum += len(user_code) * num
+		complexity_sum += len(instructions) * num
 	}
 
 	fmt.Printf("Complexity sum: %d\n", complexity_sum)
@@ -154,6 +160,10 @@ func numToDir(robot *Point, code string) string {
 			continue
 		}
 
+		// Add the higher-cost instructions first except in the above cases
+		// where the empty space would be crossed. The "<" key is the highest
+		// cost move because it takes the most steps to reach from the "A"
+		// key, for example.
 		if delta.Col < 0 {
 			for i := 0; i < delta.Col*(-1); i++ {
 				instructions += "<"
@@ -188,70 +198,102 @@ func numToDir(robot *Point, code string) string {
 	return instructions
 }
 
-func dirToDir(robot *Point, code string) string {
-	instructions := ""
+func dirToDir(robot *Point, code string, dir_memo map[string]string) string {
+	var builder strings.Builder
 	chars := strings.Split(code, "")
+	current_symbol := "A"
 
 	for _, ch := range chars {
 		delta := dir_ref[ch].Minus(*robot) // Desired position minus current position
+		next_symbol := ch
+		sub_instructions := "" // Instructions to move from current to next
+
+		// Look up step sequence for previously encountered scenarios
+		if val, ok := dir_memo[current_symbol+next_symbol]; ok {
+			builder.WriteString(val)
+			current_symbol = next_symbol
+			*robot = dir_ref[next_symbol]
+			continue
+		}
 
 		// Avoid crossing empty space in direction pad
 		if robot.Col == 0 && dir_ref[ch].Row == 0 {
 			for i := 0; i < delta.Col; i++ {
-				instructions += ">"
+				sub_instructions += ">"
 				*robot = robot.Plus(Deltas[">"])
 			}
 			for i := 0; i < delta.Row*(-1); i++ {
-				instructions += "^"
+				sub_instructions += "^"
 				*robot = robot.Plus(Deltas["^"])
 			}
-			instructions += "A"
+
+			sub_instructions += "A"
+			if _, ok := dir_memo[current_symbol+next_symbol]; !ok {
+				dir_memo[current_symbol+next_symbol] = sub_instructions
+			}
+			builder.WriteString(sub_instructions)
+			current_symbol = next_symbol
 			continue
 		}
 
 		if robot.Row == 0 && dir_ref[ch].Col == 0 {
 			for i := 0; i < delta.Row; i++ {
-				instructions += "v"
+				sub_instructions += "v"
 				*robot = robot.Plus(Deltas["v"])
 			}
 			for i := 0; i < delta.Col*(-1); i++ {
-				instructions += "<"
+				sub_instructions += "<"
 				*robot = robot.Plus(Deltas["<"])
 			}
-			instructions += "A"
+
+			sub_instructions += "A"
+			if _, ok := dir_memo[current_symbol+next_symbol]; !ok {
+				dir_memo[current_symbol+next_symbol] = sub_instructions
+			}
+			builder.WriteString(sub_instructions)
+			current_symbol = next_symbol
 			continue
 		}
 
+		// Add the higher-cost instructions first except in the above cases
+		// where the empty space would be crossed. The "<" key is the highest
+		// cost move because it takes the most steps to reach from the "A"
+		// key, for example.
 		if delta.Col < 0 {
 			for i := 0; i < delta.Col*(-1); i++ {
-				instructions += "<"
+				sub_instructions += "<"
 				*robot = robot.Plus(Deltas["<"])
 			}
 		}
 
 		if delta.Row > 0 {
 			for i := 0; i < delta.Row; i++ {
-				instructions += "v"
+				sub_instructions += "v"
 				*robot = robot.Plus(Deltas["v"])
 			}
 		}
 
 		if delta.Row < 0 {
 			for i := 0; i < delta.Row*(-1); i++ {
-				instructions += "^"
+				sub_instructions += "^"
 				*robot = robot.Plus(Deltas["^"])
 			}
 		}
 
 		if delta.Col > 0 {
 			for i := 0; i < delta.Col; i++ {
-				instructions += ">"
+				sub_instructions += ">"
 				*robot = robot.Plus(Deltas[">"])
 			}
 		}
 
-		instructions += "A"
+		sub_instructions += "A"
+		if _, ok := dir_memo[current_symbol+next_symbol]; !ok {
+			dir_memo[current_symbol+next_symbol] = sub_instructions
+		}
+		builder.WriteString(sub_instructions)
+		current_symbol = next_symbol
 	}
 
-	return instructions
+	return builder.String()
 }
